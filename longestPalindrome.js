ac92dbae61affc1ad7f90e3977a19e355937fae4
“Young <hogusong1@gmail.com>
2019-05-21 16:31:00 -0400

    Find the longest palindromic substring in the given string.

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,3 +1,13 @@
+/*
+  Given a string s, find the longest palindromic substring in s. 
+  You may assume that the maximum length of s is 1000.
+
+  Input: "babad",    Output: "bab"
+  Note: "aba" is also a valid answer.
+
+  Input: "cbbd",     Output: "bb"
+*/
+
 function longestPalindromeBF(s) {
   if (s.length < 2) return s;
   let answer = [0,0];
