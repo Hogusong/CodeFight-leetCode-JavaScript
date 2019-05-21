3b2237789e37944828f6d8dd7aa75631097dd2aa
“Young <hogusong1@gmail.com>
2019-05-21 12:11:15 -0400

    Add 2 given numbers as array.

--- addTwoLists.js
+++ addTwoLists.js
@@ -1,44 +1,26 @@
 var addTwoNumbers = function(l1, l2) {
-  let s1 = l1.length-1;
-  let s2 = l2.length-1;
-  let up = 0;
-  let result = []
-  while (s1 > -1 && s2 > -1) {
-      const sum = l1[s1] + l2[s2] + up;
-      if (sum > 9) {
-          result.push(sum - 10);
-          up = 1;
-      } else {
-          result.push(sum);
-          up = 0
-      }
-      s1--;
-      s2--;
-  }
-  if (s1 > -1) {
-      result = addRest(l1, s1, up, [...result])
-  }
-  if (s2 > -1) {
-      result = addRest(l2, s2, up, [...result])
-  }
-  return result;
+    let s1 = l1 ? l1.length-1 : -1;
+    let s2 = l2 ? l2.length-1 : -1;
+    let up = 0;
+    let result = []
+    while (s1 >= 0 || s2 >= 0) {
+        const sum = (s1 >= 0 ? l1[s1] : 0) + (s2 >= 0 ? l2[s2] : 0) + up;
+        if (sum > 9) {
+        result.push(sum - 10);
+        up = 1;
+        } else {
+        result.push(sum);
+        up = 0
+        }
+        s1--;
+        s2--;
+    }
+    if (up > 0) {
+        result.push(up);
+    }
+    return result.reverse();
 };
 
-function addRest(arr, start, up, answer) {
-  for (let i=start; i>-1; i--) {
-      const sum = arr[i] + up;
-      if (sum > 9) {
-          answer.push(sum - 10);
-          up = 1;
-      } else {
-          answer.push(sum);
-          up = 0
-      }
-  }
-  if (up > 0) {
-      answer.push(up)
-  }
-  return answer;
-}
-
+console.log(addTwoNumbers([1,9,5], [1, 5]));
 console.log(addTwoNumbers([5], [5]));
+console.log(addTwoNumbers([5], null));
