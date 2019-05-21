4629fd5278a8aa261df7b1a9ae8275cb01f94819
“Young <hogusong1@gmail.com>
2019-05-21 16:20:21 -0400

    Find the median of the two sorted arrays.

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -1,3 +1,16 @@
+/*
+  There are two sorted arrays nums1 and nums2 of size m and n respectively.
+  Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+  You may assume nums1 and nums2 cannot be both empty.
+
+  nums1 = [1, 3],   nums2 = [2]
+  The median is 2.0
+
+  nums1 = [1, 2],   nums2 = [3, 4]
+  The median is (2 + 3)/2 = 2.5
+*/
+
 function xgetMedian(nums1, nums2) {
   let i = 0, j = 0;
   let combined = [];
