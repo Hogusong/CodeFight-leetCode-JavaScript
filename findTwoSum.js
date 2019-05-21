8dd8d6fca577958d8150365b255e053d66082290
“Young <hogusong1@gmail.com>
2019-05-21 18:11:44 -0400

    Find a set in a given array to make sum === target.

--- findTwoSum.js
+++ findTwoSum.js
@@ -1,15 +1,15 @@
 /*
-Given an array of integers, return the first indices of the two numbers 
-such that they add up to a specific target.
-You may assume that each input would have exactly one solution, 
-and you may not use the same element twice.
+  Given an array of integers, return the first indices of the two numbers 
+  such that they add up to a specific target.
+  You may assume that each input would have exactly one solution, 
+  and you may not use the same element twice.
 
-Example:
-Given nums = [1, 2, 11, 7, 8, 15], target = 9,
+  Example:
+  Given nums = [1, 2, 11, 7, 8, 15], target = 9,
 
-Because nums[1] + nums[3] = 2 + 7 = 9,
-return [1, 3]
-num[0] + num[4] = 1 + 8 = 9 but this set will come later than the other.
+  Because nums[1] + nums[3] = 2 + 7 = 9,
+  return [1, 3]
+  num[0] + num[4] = 1 + 8 = 9 but this set will come later than the other.
 */
 
 function findTwoSum(arr, target) {
