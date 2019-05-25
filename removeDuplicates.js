4fc664d6f97a4271baa7b89c6b2db0c84b24c13c
“Young <hogusong1@gmail.com>
2019-05-25 11:56:09 -0400

    Remove all duplicates in given sorted array and return the new length.

---
+++ removeDuplicates.js
@@ -0,0 +1,32 @@
+/*
+  Given a sorted array nums, remove the duplicates in-place such that each element appear only once 
+  and return the new length. Do not allocate extra space for another array, 
+  you must do this by modifying the input array in-place with O(1) extra memory.
+
+  Given nums = [1,1,2],                 return 2  -->  1 and 2 respectively.
+  Given nums = [0,0,1,1,1,2,2,3,3,4],   return 5  -->  0, 1, 2, 3, and 4 respectively.
+
+  Clarification:
+  Confused why the returned value is an integer but your answer is an array?
+
+  Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+  Internally you can think of this:
+  // nums is passed in by reference. (i.e., without making a copy)
+  int len = removeDuplicates(nums);
+  // any modification to nums in your function would be known by the caller.
+  // using the length returned by your function, it prints the first len elements.
+  for (int i = 0; i < len; i++) {
+      print(nums[i]);
+  }
+*/
+
+function removeDuplicates(nums) {
+  if (nums.length === 0) return 0;
+  let index = 0;
+  for (let j=1; j<nums.length; j++) {
+      if (nums[j] != nums[index]) {
+          nums[++index] = nums[j]
+      }
+  }
+  return ++index;
+}
