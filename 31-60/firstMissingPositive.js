ed63498731d8beec2146c26a1cac78012eb4231f
“Young <hogusong1@gmail.com>
2019-05-29 16:57:52 -0400

    Find the smallest missing positive integer from an unsorted array.

---
+++ 31-60/firstMissingPositive.js
@@ -0,0 +1,39 @@
+/*
+  Given an unsorted integer array, find the smallest missing positive integer.
+
+  Input: [1,2,0]          Output: 3 
+  Input: [3,4,-1,1]       Output: 2
+  Input: [7,8,9,11,12]    Output: 1
+
+  Your algorithm should run in O(n) time and uses constant extra space.
+*/
+
+function firstMissingPositive(nums) {
+  if (nums.length < 1) return 1;
+  if (nums.length === 1) return (nums[0] != 1 ) ? 1 : 2;
+
+  const arr = nums.sort((a,b) => a - b);
+  const idx = findFirstPositive(arr);
+  if (idx < 0) return 1;
+  if (arr[idx] > 1) return 1;
+  for (let i=idx+1; i<arr.length; i++) {
+    if (arr[i] - arr[i-1] > 1) return arr[i-1] + 1;
+  }
+  return arr[arr.length-1] + 1;
+}
+
+function findFirstPositive(arr) {
+  if (arr[0] > 0) return 0;
+  if (arr[arr.length-1] < 1) return -1;
+  let start = 0, end = arr.length - 1;
+  while(start <= end) {
+    const c = Math.floor((start + end) / 2);
+    if (arr[c] > 0 && (!arr[c-1] || arr[c-1] < 1)) return c;
+    if (arr[c] < 1) start = c + 1;
+    else end = c - 1;
+  }
+}
+
+console.log(firstMissingPositive([3,4,8,10,12]));
+console.log(firstMissingPositive([1,2,0]));
+console.log(firstMissingPositive([3,5,-1,0,1,-2]));
