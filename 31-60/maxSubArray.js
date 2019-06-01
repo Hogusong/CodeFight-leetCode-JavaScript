cb80f00273ec0e600a6b3e0df464d4af416319ff
“Young <hogusong1@gmail.com>
2019-05-31 20:49:43 -0400

    Find the contiguous subarray which has the largest sum(Using Divide and Conquer).

--- 31-60/maxSubArray.js
+++ 31-60/maxSubArray.js
@@ -38,3 +38,34 @@ console.log(maxSubArrayBF([-2,1,-3,4,-1,2,1,-5,4]))
 console.log(maxSubArray([-1,0,-2]));
 console.log(maxSubArray([-1,-2]));
 console.log(maxSubArray([-2]));
+
+//  Divide and Conquer
+function maxSubArrayDC(nums) {
+  return maxSubArrayRec(nums, 0, nums.length-1);
+}
+
+function maxCrossingSum(arr, s, m, e) {
+  let sum = 0; left_sum = null, right_sum = null;
+  for (let i=m; i>=s; i--) {
+    sum = sum + arr[i];
+    left_sum = (left_sum === null) ? sum : Math.max(sum, left_sum);
+  }
+
+  sum = 0;
+  for (let i=m+1; i<=e; i++) {
+    sum = sum + arr[i];
+    right_sum = (right_sum === null) ? sum : Math.max(sum, right_sum);
+  }
+  return left_sum + right_sum;
+}
+
+function maxSubArrayRec(arr, s, e) {
+  if ( s === e) return arr[s];
+  const m = Math.floor((s + e) / 2);
+  return Math.max(maxSubArrayRec(arr, s, m), maxSubArrayRec(arr, m+1, e), maxCrossingSum(arr, s, m, e));
+}
+
+console.log(maxSubArrayDC([-2,1,-3,4,-1,2,1,-5,4]))
+console.log(maxSubArrayDC([-1,0,-2]));
+console.log(maxSubArrayDC([-1,-2]));
+console.log(maxSubArrayDC([-2]));
