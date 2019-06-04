763014ef0865a4cbd1cb2b62123de060c4a6c1a5
“Young <hogusong1@gmail.com>
2019-06-04 13:53:59 -0400

    Set its entire row and column to 0 if an element is 0. (Space Efficient)

--- 61-90/setZeroes.js
+++ 61-90/setZeroes.js
@@ -56,3 +56,53 @@ input = [ [0,1,2,0],
           [3,4,5,2],          
           [1,3,1,5] ]         
 console.log(setZeroesBFO1(input));
+
+//  Space Efficient Solution
+function setZeroesEff(matrix) {
+  let isCol = false;
+  let R = matrix.length;
+  let C = matrix[0].length;
+
+  for (let i = 0; i < R; i++) {
+    if (matrix[i][0] == 0) isCol = true;
+    for (let j = 1; j < C; j++) {
+      // If an element is zero, we set the first element of the corresponding row and column to 0
+      if (matrix[i][j] == 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+  console.log(matrix.join('\n'))
+  // Iterate over the array once again and using the first row and first column, update the elements.
+  for (let i = 1; i < R; i++) {
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  console.log('\n' + matrix.join('\n'))
+
+  // See if the first row needs to be set to zero as well
+  if (matrix[0][0] == 0) {
+    for (let j = 0; j < C; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+  console.log('\n' + matrix.join('\n'))
+
+  // See if the first column needs to be set to zero as well
+  if (isCol) {
+    for (let i = 0; i < R; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+  console.log('\n' + matrix.join('\n'))
+  return matrix;
+}
+
+input = [ [1,0,2,0], 
+          [3,4,5,2],          
+          [1,3,1,5] ]         
+console.log(setZeroesEff(input));
