dbf1d8f31fcf0aa16951c5ec628d97ab141e234a
“Young <hogusong1@gmail.com>
2019-06-04 00:23:42 -0400

    Find minimum changes to convert word1 -> word2. (Divide and Conquer)

---
+++ 61-90/minDistance.js
@@ -0,0 +1,49 @@
+/*
+  Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+  You have the following 3 operations permitted on a word:
+      Insert a character,   Delete a character,   Replace a character
+
+  Input: word1 = "horse", word2 = "ros",              Output: 3
+      horse -> rorse (replace 'h' with 'r')
+      rorse -> rose (remove 'r')
+      rose -> ros (remove 'e')
+  Input: word1 = "intention", word2 = "execution",    Output: 5
+      intention -> inention (remove 't')
+      inention -> enention (replace 'i' with 'e')
+      enention -> exention (replace 'n' with 'x')
+      exention -> exection (replace 'n' with 'c')
+      exection -> execution (insert 'u')
+*/
+
+//  Divide and Conquer
+function minDistanceDC(word1, word2) {
+  return editDist(word1, word2, word1.length, word2.length);
+}
+
+function editDist(W1 , W2 , m, n) { 
+  // If first string is empty, the only option is to 
+	// insert all characters of second string into first 
+	if (m == 0) return n; 
+	
+	// If second string is empty, the only option is to 
+	// remove all characters of first string 
+	if (n == 0) return m; 
+	
+	// If last characters of two strings are same, nothing 
+	// much to do. Ignore last characters and get count for 
+	// remaining strings. 
+	if (W1[m-1] === W2[n-1]) return editDist(W1, W2, m-1, n-1); 
+	
+	// If last characters are not same, consider all three 
+	// operations on last character of first string, recursively 
+	// compute minimum cost for all three operations and take 
+	// minimum of three values. 
+	return 1 + Math.min(editDist(W1, W2, m, n-1), // Insert
+					editDist(W1, W2, m-1, n),     // Remove
+					editDist(W1, W2, m-1, n-1));  // Replace
+}
+
+w1 = 'sunday';
+w2 = 'saturday';
+console.log(minDistanceDC(w1, w2));
