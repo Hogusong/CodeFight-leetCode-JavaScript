67008ed91d8fadb6d90e4de0b9f4abd27a8422f3
“Young <hogusong1@gmail.com>
2019-06-05 14:06:23 -0400

    Find an exist for the word in a given m x n board.

---
+++ 61-90/findExit.js
@@ -0,0 +1,105 @@
+/*
+  Given a 2D board and a word, find if the word exists in the grid.
+
+  The word can be constructed from letters of sequentially adjacent cell, 
+  where "adjacent" cells are those horizontally or vertically neighboring. 
+  The same letter cell may not be used more than once.
+
+  Example:
+
+  board =
+  [
+    ['A','B','C','E'],
+    ['S','F','C','S'],
+    ['A','D','E','E']
+  ]
+
+  Given word = "ABCCED", return true.
+  Given word = "SEE", return true.
+  Given word = "ABCB", return false.
+*/
+
+function findExit(board, word) {
+  if (word === '') return false;
+  if (board.length < 1 || board[0].length < 1) return false;
+  const start = findStartingPoint(board, word[0]);
+  if (start.length < 1) return false;
+  for (let S of start) {
+    // if (findPathList(board, word.slice(1), S, [])) return true;
+    if (findPathObj(board, word.slice(1), S, {})) return true;
+  }
+  return false;
+}
+
+function findPathList(B, W, S, dict) {
+  dict.push(S.join(':'));
+  if (!W) return true;
+  let r = S[0], c = S[1], rows = B.length, cols = B[0].length;
+  const next = [];
+  if (r-1 >= 0 && B[r-1][c] === W[0] && !dict.includes((r-1)+':'+c)) next.push([r-1, c]);
+
+  if (r+1 < rows && B[r+1][c] === W[0] && !dict.includes((r+1)+':'+c)) next.push([r+1, c]);
+
+  if (c-1 >= 0 && B[r][c-1] === W[0] && !dict.includes(r+':'+(c-1))) next.push([r, c-1]);
+
+  if (c+1 < cols && B[r][c+1] === W[0] && !dict.includes(r+':'+(c+1))) next.push([r, c+1]);
+
+  for (let N of next) {
+    const found = findPathList(B, W.slice(1), N, [...dict]);
+    if (found) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function findPathObj(B, W, S, dict) {
+  dict[S.join(':')] = 1;
+  if (!W) return true;
+  let r = S[0], c = S[1], rows = B.length, cols = B[0].length;
+  const next = [];
+  if (r-1 >= 0 && B[r-1][c] === W[0] && !dict[(r-1)+':'+c]) next.push([r-1, c]);
+
+  if (r+1 < rows && B[r+1][c] === W[0] && !dict[(r+1)+':'+c]) next.push([r+1, c]);
+
+  if (c-1 >= 0 && B[r][c-1] === W[0] && !dict[r+':'+(c-1)]) next.push([r, c-1]);
+
+  if (c+1 < cols && B[r][c+1] === W[0] && !dict[r+':'+(c+1)]) next.push([r, c+1]);
+
+  for (let N of next) {
+    const found = findPathObj(B, W.slice(1), N, Object.assign({}, dict));
+    if (found) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function findStartingPoint(board, s) {
+  const ans = [];
+  for (let r = 0; r < board.length; r++) {
+    let index = -1;
+    while(true) {
+      index = board[r].indexOf(s, index+1);
+      if (index >= 0) ans.push([r, index]);
+      else break;
+    }
+  }
+  return ans;
+}
+
+board = [ ['A','B','C','E'],
+          ['S','F','C','S'],
+          ['A','D','E','E'] ];
+words = ["ABCCED", "SEE", "ABCB"];
+for (let word of words) console.log(findExit(board, word)); // true, true, false
+board = [ ["A","B","C","E"],
+          ["S","F","E","S"],
+          ["A","D","E","E"] ];
+word = "ABCESEEEFS";
+console.log(findExit(board, word));   // true
+board = [ ["a","a","a","a"],
+          ["a","a","a","a"],
+          ["a","a","a","a"] ]
+word = "aaaaaaaaaaaaa";
+console.log(findExit(board, word));   //  false
