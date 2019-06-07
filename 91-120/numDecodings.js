c550fcbf2ba571e726451d51d41a5da69901da94
“Young <hogusong1@gmail.com>
2019-06-07 16:35:03 -0400

    Count valid possible decoding of a given string.(Object & Recursion)

--- 91-120/numDecodings.js
+++ 91-120/numDecodings.js
@@ -42,3 +42,36 @@ function countPossibleDecodings(str, i) {
 
 Strings = ['123456', '1502658', '226'];
 for (let s of Strings) console.log(numDecodings(s));
+
+function numDecodingsObj(s) {
+  if (s.length < 1 || s[0] === '0') return 0;
+
+  dict = {};
+  return countPossibleRec(s, 0);
+}
+
+function countPossibleRec(str, i) {
+  if (str[i] === '0') return 0;
+
+  if (i === str.length - 1) {
+    return 1;
+  }
+
+  let x = 0, y = 0;
+  if (i + 1 === str.length - 1) {
+    if (+str.substr(i, 2) <= 26) x = 1
+    if (!dict[i+1]) dict[i+1] = countPossibleRec(str, i+1);
+    return dict[i+1] + x
+  }
+
+  if (!dict[i+1]) dict[i+1] = countPossibleRec(str, i+1);
+  x = dict[i+1]
+
+  if (i+2 < str.length && +str.substr(i, 2) <= 26) {
+    if (!dict[i+1]) dict[i+2] = countPossibleRec(str, i+2);
+    y = dict[i+2] ;
+  }
+  return x + y;
+}
+
+for (let s of Strings) console.log(numDecodingsObj(s));
