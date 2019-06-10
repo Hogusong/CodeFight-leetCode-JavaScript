ade065221e2d865e58cf41d62550efeb007ac6b0
“Young <hogusong1@gmail.com>
2019-06-10 00:08:06 -0400

    Find a given binary tree is a valid BST or not.(Recursion)

---
+++ 91-120/isValidBST.js
@@ -0,0 +1,49 @@
+/*
+  Given a binary tree, determine if it is a valid binary search tree (BST).
+  Assume a BST is defined as follows:
+
+  The left subtree of a node contains only nodes with keys less than the node's key.
+  The right subtree of a node contains only nodes with keys greater than the node's key.
+  Both the left and right subtrees must also be binary search trees.
+     2
+    / \
+   1   3
+  Input: [2,1,3]                      Output: true
+     5
+    / \
+   1   4
+      / \
+     3   6
+  Input: [5,1,4,null,null,3,6]        Output: false
+  Explanation: The root node's value is 5 but its right child's value is 4.
+*/
+
+class TreeNode {
+  constructor(v, l=null, r=null) {
+    this.val = v;
+    this.left = l;
+    this.right = r;
+  }
+}
+t3 = new TreeNode(3);
+t6 = new TreeNode(6);
+t4 = new TreeNode(4, t3, t6);
+t1 = new TreeNode(1);
+t5 = new TreeNode(5, t1, t4);
+
+function isValidBST(root) {
+  return isValidRec(root, null, null);
+}
+
+function isValidRec(node, lower, upper) {
+  if (!node) return true;
+  let val = node.val;
+  if (lower != null && val <= lower) return false;
+  if (upper != null && val >= upper) return false;
+
+  const right = isValidRec(node.right, val, upper);
+  const left = isValidRec(node.left, lower, val);
+  return right && left;
+}
+
+console.log(isValidBST(t5));
