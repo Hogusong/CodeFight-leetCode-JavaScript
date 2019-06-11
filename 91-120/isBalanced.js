a938f78876a355b9f31f7b26043f92e75585a8da
“Young <hogusong1@gmail.com>
2019-06-11 12:25:28 -0400

    Determine if a given binary tree is height-balanced.(Optimized Implementation)

--- 91-120/isBalanced.js
+++ 91-120/isBalanced.js
@@ -34,8 +34,31 @@ function height(node) {
   return 1 + Math.max(height(node.left), height(node.right));
 }
 
+//  Optimized implementation
+class Height {
+  constructor() {
+    this.height = 0
+  }
+}
 
-//  Just compare Max & Min depth
+function isBalancedOpt(root) {
+  if (!root) return true;
+  return checkBalance(root, new Height());
+}
+
+function checkBalance(node, height) {
+  if (!node) return true;
+  let l_height = new Height(),  r_height = new Height();
+  let L = checkBalance(node.left, l_height);
+  let R = checkBalance(node.right, r_height);
+  let lh = l_height.height,  rh = r_height.height;
+  console.log(lh, rh);
+  height.height = (lh > rh ? lh : rh) + 1;
+  if (Math.abs(lh - rh) > 1) return false;
+  return L && R
+}
+
+//  Just find Max & Min depth of the root.
 function countDepth(root) {
   if (!root) return true;
   maxDepth = 0, minDepth = 1000000;
