dfce3a2f60caf498c46ac2f2fceb50329e3e882f
“Young <hogusong1@gmail.com>
2019-06-12 02:07:25 -0400

    Set next node for all nodes of a given binary tree.(Recursion)

---
+++ 91-120/conect2.js
@@ -0,0 +1,56 @@
+/*
+  Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+  Initially, all next pointers are set to NULL.
+
+  
+            1               1  --> null
+          /   \           /   \
+        2       3       2  -->  3 --> null
+       / \       \     / \       \
+      4   5       7   4 ->5   ->  7 --> null 
+
+  Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+*/
+
+//  Recursion
+function connect(root) {
+  if (!root) return null;
+  root.next = null
+  connectRec(root);
+  return root;
+};
+
+function connectRec(node) {
+  if (!node) return;
+
+  if (node.next) connectRec(node.next);
+  
+  if (node.left) {
+      if (node.right) {
+          node.left.next = node.right;
+          node.right.next = getNext(node.next);
+      } else {
+          node.left.next = getNext(node.next);
+      }
+      connectRec(node.left);
+  } else if (node.right) {
+      node.right.next = getNext(node.next);
+      connectRec(node.right);
+  }
+}
+
+function getNext(node) {
+  if (!node) return null;
+  if (node.left) return node.left;
+  if (node.right) return node.right;
+  return getNext(node.next)
+}
+
+// function getNext(node) {
+//   while (node) {
+//     if (node.left) return node.left;
+//     if (node.right) return node.right;
+//     node = node.next
+//   }
+//   return null;
+// }
