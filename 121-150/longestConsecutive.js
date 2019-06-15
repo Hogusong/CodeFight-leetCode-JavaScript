9dea3176fc411bdf3a7ffd3b383e1bbf944f7074
“Young <hogusong1@gmail.com>
2019-06-14 22:56:09 -0400

    Find the length of the longest consecutive elements sequence in a given unsorted array.(Using Set)

--- 121-150/longestConsecutive.js
+++ 121-150/longestConsecutive.js
@@ -24,3 +24,30 @@ function longestConsecutive(nums) {
 
 N = [100, 4, 200, 1, 3, 2, 3];
 console.log(longestConsecutive(N));
+
+function longestConsecutiveSet(nums) {
+  const num_set = new Set();
+  for (let num of nums) {
+      num_set.add(num);
+  }
+
+  let longestStreak = 0;
+
+  for (let num of num_set) {
+    if (!num_set.has(num-1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (num_set.has(currentNum+1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+}
+
+N = [100, 4, 200, 1, 3, 2, 3];
+console.log(longestConsecutiveSet(N));
