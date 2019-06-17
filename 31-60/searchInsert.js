12ecb939c697a0404161632ba8a8eefc1d50200e
“Young <hogusong1@gmail.com>
2019-06-17 00:26:05 -0400

    Find insert position or target element in a given sorted array.

--- 31-60/searchInsert.js
+++ 31-60/searchInsert.js
@@ -27,3 +27,19 @@ console.log(searchInsert([1,3,5,6], 2));
 console.log(searchInsert([1,3,5,6], 7));
 console.log(searchInsert([1,3,5,6], 0));
 console.log(searchInsert([3,5,7,9,10], 8));
+
+function searchInsert(nums, target) {
+  if (nums.length < 1) return 0;
+
+  let from = 0, to = nums.length - 1;
+  while (to >= from) {
+    const center = Math.floor((from + to) / 2);
+    if (nums[center] === target) return center;
+    if (nums[center] > target) {
+      to = center - 1;
+    } else {
+      from = center + 1;
+    }
+  }
+  return from;
+}
