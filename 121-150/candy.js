5e64f13f6237a39002d77ceac2ce3eef0f614298
“Young <hogusong1@gmail.com>
2019-06-23 21:45:22 -0400

    Count candies to give to all children by the given rules.(DP)

--- 121-150/candy.js
+++ 121-150/candy.js
@@ -41,3 +41,26 @@ function giveCandy(ratings) {
 
 ratings = [3,4,3,2,1,2,6,4,3,1]
 console.log(giveCandy(ratings));
+
+function candy(ratings) {
+  const len = ratings.length;
+  let candies = [];
+  for (let i = 0; i < len; i++) candies[i] = 1;
+
+  for (let i = 1; i < len; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  let sum = candies[len - 1];
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+    }
+    sum += candies[i];
+  }
+  return sum;
+}
+
+console.log(countCandies(ratings));
