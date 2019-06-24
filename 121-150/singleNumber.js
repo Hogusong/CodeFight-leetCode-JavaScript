2a88d74b026db2bb4c66a4a1c79d259e4b16abfa
“Young <hogusong1@gmail.com>
2019-06-24 01:20:32 -0400

    Find that single one. Every element appears twice except one.(Using Object)

--- 121-150/singleNumber.js
+++ 121-150/singleNumber.js
@@ -40,3 +40,13 @@ function singleNumber(nums) {
   }
   return 2 * sumSet - sumArray;
 }
+
+//  Using hash-table
+function singleNumber(nums) {
+  dict = {};
+  for (let n of nums) {
+      if (dict[n]) delete(dict[n]);
+      else dict[n] = 1;
+  }
+  return Object.keys(dict)[0];
+}
