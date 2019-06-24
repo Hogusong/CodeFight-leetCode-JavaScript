9f637d0c5668b80d2f3749ede4706e183911a65a
“Young <hogusong1@gmail.com>
2019-06-24 00:37:17 -0400

    Find that single one. Every element appears twice except one.(BF)

---
+++ 121-150/singleNumber.js
@@ -0,0 +1,19 @@
+/*
+  Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+  Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+  Input: [2,2,1],             Output: 1
+  Input: [4,1,2,1,2]          Output: 4
+*/
+
+// Brute Force
+function singleNumber(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == null) continue;
+    const index = nums.indexOf(nums[i], i+1);
+    if (index < 0) return nums[i];
+    nums[i] = null;
+    nums[index] = null;
+  }
+  return null;
+}
