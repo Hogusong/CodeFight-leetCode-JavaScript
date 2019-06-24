c5c8be5da0d248b11736099ede72526c8e8b5b77
“Young <hogusong1@gmail.com>
2019-06-24 00:51:50 -0400

    Find that single one. Every element appears twice except one.(Sorting)

--- 121-150/singleNumber.js
+++ 121-150/singleNumber.js
@@ -17,3 +17,12 @@ function singleNumber(nums) {
   }
   return null;
 }
+
+// Sorting first
+function singleNumber(nums) {
+  nums = nums.sort((a,b) => a-b);
+  for (let i = 0; i < nums.length - 1; i+=2 ) {
+    if (nums[i] != nums[i+1]) return nums[i];
+  }
+  return nums[nums.length - 1];
+}
