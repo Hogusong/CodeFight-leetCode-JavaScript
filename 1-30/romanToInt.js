e08430f199cc66e33e31ce08f5b60879430aa6ae
“Young <hogusong1@gmail.com>
2019-11-21 14:54:37 -0500

    Update - 3

--- 1-30/romanToInt.js
+++ 1-30/romanToInt.js
@@ -15,4 +15,14 @@ function romanToInt(str) {
   return answer + N[idx];
 }
 
+var romanToInt = function(s) {
+  const dict = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
+  let sum = 0, i = 0;
+  for ( i ; i < s.length - 1; i++) {
+    if (dict[s[i]] < dict[s[i+1]]) sum -= dict[s[i]];
+    else sum += dict[s[i]];
+  }
+  return sum + dict[s[i]]
+}
+
 console.log(romanToInt('CMXCIX'));
--- 1-30/threeSum.js
+++ 1-30/threeSum.js
@@ -8,10 +8,9 @@
   A solution set is: [ [-1, 0, 1], [-1, -1, 2] ]
 */
 
-function threeSum(nums) {
+function threeSumRec(nums) {
   subsets = [];
-  const data = nums.map((e,i) => i);
-  getSubsets(nums, nums.length, 3, 0, data, 0);
+  processRec(nums, nums.length, 3, 0, []);
   const result = []
   for (let s of subsets) {
     if (s[0] + s[1] + s[2] === 0) {
@@ -28,87 +27,48 @@ function threeSum(nums) {
   return result.length;
 }
 
-function getSubsets(arr, n, r, index, data, i) {
-  if (index === r) {
-    const temp = []
-    for (let j=0; j<r; j++) {
-      temp.push(data[j]);
-    }
-    subsets.push(temp)
-    return
+function processRec(arr, n, r, i, data) {
+  if (data.length === r) {
+    subsets.push(data);
+    return;
   }
-  if (i >= n) return
-
-  data[index] = arr[i];
-  getSubsets(arr, n, r, index+1, data, i+1);
-  getSubsets(arr, n, r, index, data, i+1);
+  if (i >= n) return;
+  processRec(arr, n, r, i+1, [...data, arr[i]]);
+  processRec(arr, n, r, i+1, [...data]);
 }
 
 // nums = [-13,10,11,-3,8,11,-4,8,12,-13,5,-6,-4,-2,12,11,7,-7,-3,10,12,13,-3,-2,6,-1,14,7,-13,8,14,-10,-4,10,-6,11,-2,-3,4,-13,0,-14,-3,3,-9,-6,-9,13,-6,3,1,-9,-6,13,-4,-15,-11,-12,7,-9,3,-2,-12,6,-15,-10,2,-2,-6,13,1,9,14,5,-11,-10,14,-5,11,-6,6,-3,-8,-15,-13,-4,7,13,-1,-9,11,-13,-4,-15,9,-4,12,-4,1,-9,-5,9,8,-14,-1,4,14];
 nums = [14,4,6,-1,10,9,-8,7,-13,14,-13,-11,-8,-9,11,14,-8,-14,-13,7,-10,-15,-13,-11,-11,11,14,13,2,-14,1,-7,-2,14,-1,-15,9,7,-1,3,6,1,7,5,-1,-5,4,-2,-4,-1,-9,-7,-1,-7,-11,3,12,10,-7,-1,12,1,8,-13,1,14,9,-13,6,-7,-3,-11,2,-11,10,-14,-1,-9,0,2,5,6,3,-11,6,7,0,3,3,0,-12,-8,-13,3,-14,-5,2,10,-11,-14,-12,1,-10,5,5,7,-1,11,14,6,-10,-4,-3,8,-7,10,1,8,-1,-11,-15,-6,-12,-13,12,-11];
 // nums = [-1, 2, 0, 1, -1, -3, 4];
 console.time('This')
-console.log(threeSum(nums));
+console.log(threeSumRec(nums));
 console.timeEnd('This')
-
-function threeSubset(nums) {
-  const len = nums.length
-  const arr = []
-  for (let i=0; i<len; i++) {
-    arr.push(i);
-  }
-  let ans = [];
-  let temp = [];
-  arr.forEach(e => temp.push([e]))
-  for (let i=0; i<arr.length; i++) {
-    for (let j=i+1; j<temp.length; j++) {
-      if (arr[i] != temp[j][0]) {
-        ans.push([arr[i], temp[j][0]])
-      }
-    }
-  }
-  temp = [...ans];
-  ans = [];
-  for (let i=0; i<arr.length; i++) {
-    for (let j=0; j<temp.length; j++) {
-      if (arr[i] > temp[j][0] && arr[i] > temp[j][1]) {
-        ans.push([arr[i], ...temp[j]])
-      }
-    }
-  }
-  return ans;
-}
-
-// console.time('This')
-// console.log(threeSubset(nums));
-// console.timeEnd('This')
 
 function threeSubsetSum(nums) {
   if (nums.length < 3) return [];
 
-  const result = [], dict = {};
+  nums = nums.sort((a, b) => a - b);
+  const result = [];
   for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] == nums[i-1]) continue;
+    const set = new Set();
+
     for (let j = i+1; j < nums.length - 1; j++) {
+      if (j > i + 1 && nums[j] == nums[j-1]) continue;
       const diff = 0 - nums[i] - nums[j];
+      if (diff < 0) break;
       const index = nums.indexOf(diff, j + 1);
       if (index > j) {
-        const arr = [nums[i], nums[j], nums[index]].sort((a,b) => a-b);
-        const key = arr[0] + ':' + arr[1] + ':' + arr[2]
-        if (!dict[key]) {
-            dict[key] = 1;
-            result.push(arr);
+        const key = nums[j] + ':' + nums[index]
+        if (!set.has(key)) {
+            set.add(key);
+            result.push([nums[i], nums[j], nums[index]]);
         }
       }
     }
   }
   return result.length;
-}
-
-function isNew(R, A) {
-  for (let r of R) {
-    if (r[0] === A[0] && r[1] === A[1] && r[2] === A[2]) return false;
-  }
-  return true;
 }
 
 console.time('This')
@@ -120,13 +80,15 @@ var threeSum = function(nums) {
   nums = nums.sort((a,b) => a - b);
   
   const result = [];
-  const set = new Set();
   for (let i = 0; i < n - 1; i++) {
+      if (nums[i] > 0) break;
+      if (i > 0 && nums[i] == nums[i-1]) continue;
+      const set = new Set();
       let l = i + 1;
       let r = n - 1;
       while (l < r) {
           if (nums[i] + nums[l] + nums[r] == 0) {
-              const key = nums[i] + ':' + nums[l] + ':' + nums[r];
+              const key = nums[l] + ':' + nums[r];
               if (!set.has(key)) {
                   set.add(key);
                   result.push([nums[i], nums[l], nums[r]]);    
@@ -144,17 +106,25 @@ console.time('This')
 console.log(threeSum(nums));
 console.timeEnd('This')
 
-var threeSum = function(nums) {
-  if (nums.length < 3) return [];
+var threeSumBF = function(nums) {
+  const n = nums.length;
+  nums = nums.sort((a,b) => a - b);
   const result = [];
-  for (let i = 0; i < nums.length; i++) {
-    const target = -nums[i];
-    const dict = {};
-    for (let j = i+1; j < nums.length; j++) {
-      const key = target - nums[j];
-      if (dict[key]) result.push([-target, key, nums[j]]);
-      dict[nums[j]] = true;
+  for (let i = 0; i < n - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i-1] == nums[i]) continue;
+
+    for (let j = i + 1; j < n - 1; j++) {
+      if (nums[i] + nums[j] > 0) break;
+      if (j > i + 1 && nums[j-1] == nums[j]) continue;
+
+      const keyValue = 0 - (nums[i] + nums[j]);
+      for (let k = j + 1; k < n; k++) {
+        if (nums[k] > keyValue) break;
+        if (k > j + 1 && nums[k-1] == nums[k]) continue;
+        if (nums[k] == keyValue) result.push([nums[i], nums[j], nums[k]]);
+      }
     }
   }
   return result;
-};
+}
